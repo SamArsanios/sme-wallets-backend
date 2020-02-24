d857ab50c9b755d449a71792a9fbe805e090cda7
[sam arsanios] <[ssamseat@hotmail.com]>
2020-02-24 08:57:44 +0300

    Change to emailVerifiedStr

--- src/main/kotlin/com/screentech/cordamigrate/controller/supplier/order/SupplierOrderController.kt
+++ src/main/kotlin/com/screentech/cordamigrate/controller/supplier/order/SupplierOrderController.kt
@@ -27,6 +27,7 @@ class SupplierOrderController : CRUDAbstract<SupplierOrder>(){
         anObject.order.buyer?.emailVerifiedAt = parseStringToTimestamp(anObject.order.buyer?.emailVerifiedAtStr)
         anObject.order.supplier?.emailVerifiedAt = parseStringToTimestamp(anObject.order.supplier?.emailVerifiedAtStr)
         anObject.order.wallet?.timestamp = parseStringToTimestamp(anObject.order.wallet?.timestampStr)
+        anObject.order.wallet.user?.emailVerifiedAt = parseStringToTimestamp(anObject.order.wallet.user?.emailVerifiedAtStr)
 
         return anObject
 
