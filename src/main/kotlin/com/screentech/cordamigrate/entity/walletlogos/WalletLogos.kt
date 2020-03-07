39e1b21087c02a8eb07e488a0d81e6e7b72da47d
[sam arsanios] <[ssamseat@hotmail.com]>
2020-03-07 10:07:56 +0300

    Add code structure for Class WalletLogos

--- src/main/kotlin/com/screentech/cordamigrate/entity/walletlogos/WalletLogos.kt
+++ src/main/kotlin/com/screentech/cordamigrate/entity/walletlogos/WalletLogos.kt
@@ -1,4 +1,18 @@
 package com.screentech.cordamigrate.entity.walletlogos
 
-class WalletLogos {
-}
+import com.screentech.cordamigrate.entity.user.User
+import com.screentech.cordamigrate.entity.wallet.Wallet
+import com.screentech.cordamigrate.utility.getCurrentTimestampSQL
+import java.sql.Timestamp
+import javax.persistence.*
+
+class WalletLogos (
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id") var id : Long,
+        @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "user_id") var buyer: User?,
+        @Column(name="logo", columnDefinition = "text") var logo: String?,
+        @Column(name = "the_timestamp", columnDefinition = "timestamp default now()") var timestamp: Timestamp? = getCurrentTimestampSQL(),
+        @Transient var timestampStr : String?
+)
+
+
+
