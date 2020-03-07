c9187ee09a6964f3da2b0c31c082a0c553220856
[sam arsanios] <[ssamseat@hotmail.com]>
2020-03-07 12:17:13 +0300

    Add code structure for Class Privacy Policies Entity

--- src/main/kotlin/com/screentech/cordamigrate/entity/privacypolicies/PrivacyPolicies.kt
+++ src/main/kotlin/com/screentech/cordamigrate/entity/privacypolicies/PrivacyPolicies.kt
@@ -1,4 +1,22 @@
 package com.screentech.cordamigrate.entity.privacypolicies
 
-class PrivacyPolicies {
-}
+import com.screentech.cordamigrate.entity.user.User
+import com.screentech.cordamigrate.utility.getCurrentTimestampSQL
+import jdk.nashorn.internal.ir.Assignment
+import java.sql.Timestamp
+import javax.persistence.*
+
+@Entity
+@Table(name = "privacy_policies")
+class PrivacyPolicies (
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id") var id : Long,
+        @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "supplier_id") var supplier: User?,
+        @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "user_id") var user: User?,
+        @Column(name = "privacy_policy", columnDefinition = "longtext") var privacyPolicy: String?,
+        @Column(name = "terms_conditions", columnDefinition = "longtext") var termsConditions: String?,
+        @Column(name = "notice_of_assignment", columnDefinition = "longtext") var noticeOfAssignment: String?,
+        @Column(name = "payment_authorization", columnDefinition = "longtext") var paymentAuthorization: String?,
+        @Column(name = "the_timestamp", columnDefinition = "timestamp default now()") var timestamp: Timestamp? = getCurrentTimestampSQL(),
+        @Transient var timestampStr : String?
+        )
+
