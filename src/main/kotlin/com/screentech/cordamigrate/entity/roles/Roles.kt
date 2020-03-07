8c76693de3c9831bc7ec62972b5fcf9a85026f05
[sam arsanios] <[ssamseat@hotmail.com]>
2020-03-07 10:53:51 +0300

    Add code structure for Class Roels Entity

--- src/main/kotlin/com/screentech/cordamigrate/entity/roles/Roles.kt
+++ src/main/kotlin/com/screentech/cordamigrate/entity/roles/Roles.kt
@@ -1,4 +1,15 @@
 package com.screentech.cordamigrate.entity.roles
 
-class Roles()
+import com.screentech.cordamigrate.utility.getCurrentTimestampSQL
+import java.sql.Timestamp
+import javax.persistence.*
+
+class Roles(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id") var id: Long,
+        @Column(name = "name", columnDefinition = "text") var name:String?,
+        @Column(name = "guard_name", columnDefinition = "text") var guardName:String?,
+        @Column(name = "the_timestamp", columnDefinition = "timestamp default now()") var timestamp: Timestamp? = getCurrentTimestampSQL(),
+        @Transient var timestampStr : String?
+)
+
 
