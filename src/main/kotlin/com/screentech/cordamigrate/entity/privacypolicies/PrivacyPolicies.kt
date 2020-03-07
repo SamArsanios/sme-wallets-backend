d6b1aeef8558c05312caa2f4b66998771bc0aae4
[sam arsanios] <[ssamseat@hotmail.com]>
2020-03-07 14:02:05 +0300

    Change datatype of some columns from varchar to text

--- src/main/kotlin/com/screentech/cordamigrate/entity/privacypolicies/PrivacyPolicies.kt
+++ src/main/kotlin/com/screentech/cordamigrate/entity/privacypolicies/PrivacyPolicies.kt
@@ -11,10 +11,10 @@ import javax.persistence.*
 class PrivacyPolicies (
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id") var id : Long,
         @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "user_id") var user: User?,
-        @Column(name = "privacy_policy", columnDefinition = "longtext") var privacyPolicy: String?,
-        @Column(name = "terms_conditions", columnDefinition = "longtext") var termsConditions: String?,
-        @Column(name = "notice_of_assignment", columnDefinition = "longtext") var noticeOfAssignment: String?,
-        @Column(name = "payment_authorization", columnDefinition = "longtext") var paymentAuthorization: String?,
+        @Column(name = "privacy_policy", columnDefinition = "text") var privacyPolicy: String?,
+        @Column(name = "terms_conditions", columnDefinition = "text") var termsConditions: String?,
+        @Column(name = "notice_of_assignment", columnDefinition = "text") var noticeOfAssignment: String?,
+        @Column(name = "payment_authorization", columnDefinition = "text") var paymentAuthorization: String?,
         @Column(name = "the_timestamp", columnDefinition = "timestamp default now()") var timestamp: Timestamp? = getCurrentTimestampSQL(),
         @Transient var timestampStr : String?
         )
