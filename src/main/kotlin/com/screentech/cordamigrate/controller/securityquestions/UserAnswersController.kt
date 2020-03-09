745b0e39ce0c89e058d77846a452e62998176d3b
[sam arsanios] <[ssamseat@hotmail.com]>
2020-03-10 01:03:15 +0300

    Add the Create/Update/Delete/findById endpoints for UserAnswers in the Controller file

--- src/main/kotlin/com/screentech/cordamigrate/controller/securityquestions/UserAnswersController.kt
+++ src/main/kotlin/com/screentech/cordamigrate/controller/securityquestions/UserAnswersController.kt
@@ -7,6 +7,7 @@ import com.screentech.cordamigrate.entity.user.User
 import com.screentech.cordamigrate.utility.CRUDAbstract
 import com.screentech.cordamigrate.utility.JSONUtilsKT
 import com.screentech.cordamigrate.utility.getCurrentTimestampSQL
+import com.screentech.cordamigrate.utility.parseStringToTimestamp
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.messaging.simp.SimpMessagingTemplate
@@ -26,7 +27,47 @@ class UserAnswersController: CRUDAbstract<UserAnswer>() {
 
         val result = JSONUtilsKT.ok(this.userAnswersRepository.save(anObject))
 
-        this.notificationMessage.convertAndSend("/topic/users/create", result)
+//        this.notificationMessage.convertAndSend("/topic/users/create", result)
+
+        return result
+    }
+
+    @PutMapping()
+    fun UpdateUserAnswer(@RequestBody userAnswer: UserAnswer) : ResponseEntity<*>{
+        userAnswer.timestamp = parseStringToTimestamp(userAnswer.timestampStr)
+
+        val result = JSONUtilsKT.ok(this.userAnswersRepository.save(userAnswer))
+
+//        this.notificationMessage.convertAndSend("/topic/users/create", result)
+        return result
+    }
+
+    @DeleteMapping("/delete")
+    fun deleteUserAnswer(@RequestBody userAnswer: UserAnswer) : ResponseEntity<*> {
+
+        val result = JSONUtilsKT.ok(this.userAnswersRepository.delete(userAnswer))
+
+//        this.notificationMessage.convertAndSend("/topic/users/delete", result)
+
+        return result
+    }
+
+    @GetMapping("/findAll")
+    override fun findAll() : ResponseEntity<*> {
+
+        val result = JSONUtilsKT.ok(this.userAnswersRepository.findAll())
+
+//        this.notificationMessage.convertAndSend("/topic/users/findAll", result)
+
+        return result
+    }
+
+    @GetMapping("/findById/{id}")
+    override fun findById(@PathVariable id: Long): ResponseEntity<*> {
+
+        val result = JSONUtilsKT.ok(this.userAnswersRepository.findById(id))
+
+//        this.notificationMessage.convertAndSend("/topic/orders/findById", result)
 
         return result
     }
