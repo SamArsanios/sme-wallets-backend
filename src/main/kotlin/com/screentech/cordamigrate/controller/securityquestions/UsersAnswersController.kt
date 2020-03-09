a78bb985b6d32c4e62f0f8342ed4c3227edea598
[sam arsanios] <[ssamseat@hotmail.com]>
2020-03-09 14:57:34 +0300

    security question and answer controllers

---
+++ src/main/kotlin/com/screentech/cordamigrate/controller/securityquestions/UsersAnswersController.kt
@@ -0,0 +1,7 @@
+package com.screentech.cordamigrate.controller.securityquestions
+
+import com.screentech.cordamigrate.entity.securityquestion.UserAnswer
+import com.screentech.cordamigrate.utility.CRUDAbstract
+
+class UsersAnswersController: CRUDAbstract<UserAnswer>() {
+}
