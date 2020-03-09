c72589fd18e97b6a87fa34d6f891975db55b76c0
[sam arsanios] <[ssamseat@hotmail.com]>
2020-03-09 13:43:31 +0300

    Add WalletUsers DAO

--- src/main/kotlin/com/screentech/cordamigrate/dao/walletusers/WalletUsers.kt
+++ src/main/kotlin/com/screentech/cordamigrate/dao/walletusers/WalletUsers.kt
@@ -1,4 +1,9 @@
 package com.screentech.cordamigrate.dao.walletusers
 
-interface WalletUsers {
+import com.screentech.cordamigrate.entity.walletusers.WalletUsers
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Service
+
+@Service
+interface WalletUsers: JpaRepository<WalletUsers, Long> {
 }
