bec52d0ef3c68e69a5df1a7a61106f34c4078f0b
[sam arsanios] <[ssamseat@hotmail.com]>
2020-03-09 13:56:36 +0300

    Add the DAO for OrderNotifications

---
+++ src/main/kotlin/com/screentech/cordamigrate/dao/ordernotifications/OrderNotifications.kt
@@ -0,0 +1,9 @@
+package com.screentech.cordamigrate.dao.ordernotifications
+
+import com.screentech.cordamigrate.entity.ordernotifications.OrderNotifications
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Service
+
+@Service
+interface OrderNotifications: JpaRepository<OrderNotifications, Long> {
+}
