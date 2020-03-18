60a3b3942bd3aed62cf2f1ab9cbc1c89c6af9e3c
[sam arsanios] <[ssamseat@hotmail.com]>
2020-03-18 12:25:23 +0300

    Convert the InvoiceDueDate String to Timestamp datatype

--- src/main/kotlin/com/screentech/cordamigrate/controller/invoices/InvoiceController.kt
+++ src/main/kotlin/com/screentech/cordamigrate/controller/invoices/InvoiceController.kt
@@ -38,6 +38,7 @@ class InvoiceController : CRUDAbstract<Invoice>() {
      */
     private fun parseTimestamps(anObject: Invoice) : Invoice {
         anObject.theTimestamp = parseStringToTimestamp(anObject.theTimestampStr) // parse theTimestamp
+        anObject.invoiceDueDate = parseStringToTimestamp(anObject.invoiceDueDateStr) // parse the Invoice Due Date
         anObject.sponsor.emailVerifiedAt = parseStringToTimestamp(anObject.sponsor.emailVerifiedAtStr) // parse sponsor (User.kt) timestamp
 //        anObject.wallet.timestamp = parseStringToTimestamp(anObject.wallet.timestampStr) // parse wallet (Wallet.kt) timestamp
 //        anObject.wallet.user?.emailVerifiedAt = parseStringToTimestamp(anObject.wallet.user?.emailVerifiedAtStr) // parse user timestamp in Wallet.kt
