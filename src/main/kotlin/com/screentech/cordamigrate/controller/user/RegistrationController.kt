089f1ba833c0462644b477a3a29a64a54fa3a623
[sam arsanios] <[ssamseat@hotmail.com]>
2020-03-19 16:01:15 +0300

    remove slash of request mapping

--- src/main/kotlin/com/screentech/cordamigrate/controller/user/RegistrationController.kt
+++ src/main/kotlin/com/screentech/cordamigrate/controller/user/RegistrationController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*
 import java.math.BigDecimal
 
 @RestController
-@RequestMapping("/api/smewallets/registrations")
+@RequestMapping("api/smewallets/registrations")
 class RegistrationController : CRUDAbstract<Registration>(){
 
     @Autowired
